eaa30fd06180ebbb44e35ebb3bebb1dbc55dede0
federicobarbano@gmail.com <F3d3r!C0pq2aui1618>
2018-09-20 22:03:44 +0200

    OK

--- src/test/java/stuff/TempTest.java
+++ src/test/java/stuff/TempTest.java
@@ -15,19 +15,22 @@ public class TempTest {
 
 	@Test
 	public void test() throws IOException {
-        LocalTime now = LocalTime.now();
-        LocalTime plusDT = LocalTime.of(2, 2, 2, 22);
-        LocalTime nowPlus = now.plus(plusDT.toNanoOfDay(), ChronoUnit.NANOS);
-        display("%s\t%d\t%d", DateTimeFormatter.ISO_TIME.format(now), now.get(ChronoField.MILLI_OF_SECOND), now.getNano());
-        display("%s\t%d\t%d", now, now.toNanoOfDay(), now.getNano());
-        display("%s\t%d\t%d", nowPlus, nowPlus.toNanoOfDay(), nowPlus.getNano());
-
-        display("%s", Instant.from(now).get(ChronoField.MILLI_OF_SECOND));
+//        LocalTime now = LocalTime.now();
+//        LocalTime plusDT = LocalTime.of(2, 2, 2, 22);
+//        LocalTime nowPlus = now.plus(plusDT.toNanoOfDay(), ChronoUnit.NANOS);
+//        display("%s\t%d\t%d", DateTimeFormatter.ISO_TIME.format(now), now.get(ChronoField.MILLI_OF_SECOND), now.getNano());
+//        display("%s\t%d\t%d", now, now.toNanoOfDay(), now.getNano());
+//        display("%s\t%d\t%d", nowPlus, nowPlus.toNanoOfDay(), nowPlus.getNano());
+//
+//        display("%s", Instant.from(now).get(ChronoField.MILLI_OF_SECOND));
 //		List<String> lines = Files.readAllLines(Config.CSV_STAGE_POSITIONS);
 //		String strLines = JkStreams.join(lines, "\n", l -> l.replaceAll("#.*", ""));
 //		String[] elems = JkStrings.splitAllFields(strLines, "}", true, false);
 //
 //		Arrays.stream(elems).forEach(JkConsole::display);
+
+        display("%s", "fede.csv".replaceAll("csv$", "deps.csv"));
+        display("%s", "fede".replaceAll("csv$", "deps.csv"));
 	}
 
 }
