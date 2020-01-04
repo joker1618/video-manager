8f47ce87bee5fca713682c91c502785f42a725ed
joker1618 <federicobarbano@gmail.com>
2020-01-04 05:47:55 +0000

    fix

--- src/main/java/xxx/joker/apps/video/manager/jfx/view/panes/ManagementPane.java
+++ src/main/java/xxx/joker/apps/video/manager/jfx/view/panes/ManagementPane.java
@@ -33,7 +33,7 @@ import xxx.joker.apps.video.manager.jfx.view.videoplayer.JfxVideoPlayer;
 import xxx.joker.libs.core.cache.JkCache;
 import xxx.joker.libs.core.datetime.JkDuration;
 import xxx.joker.libs.core.file.JkFiles;
-import xxx.joker.libs.core.javafx.JfxUtil;
+import xxx.joker.libs.javafx.JfxUtil;
 import xxx.joker.libs.core.lambda.JkStreams;
 import xxx.joker.libs.core.util.JkConvert;
 
